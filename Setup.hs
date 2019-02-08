8f92149168e1065469986ad5c4b0b58860c737ba
Isumi Feng <contact@zelinf.net>
2019-02-08 10:51:08 +0800

    Delegate C/C++ build to CMake
    Use the library 'cabal-bundle-clib' to integrate cmake into cabal

--- Setup.hs
+++ Setup.hs
@@ -1,2 +1,9 @@
-import Distribution.Simple
-main = defaultMain
+import           Development.CabalBundleCLib       (mainWithCLib)
+import           Development.CabalBundleCLib.CMake (simpleCMakeBuilder)
+
+main :: IO ()
+main = mainWithCLib
+  "csrc" -- C project root directory, relative to package root directory
+  ["leetcode_hs"] -- C library names
+  ["."] -- Direct parent dirs for built libraries, relative to build root dir.
+  simpleCMakeBuilder -- The builder
